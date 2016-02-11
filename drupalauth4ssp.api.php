7fdec72977a842a8981ef740b9a6561e728b7558
Alexander Schedrov <alexander.schedrov@gmail.com>
2016-02-11 15:57:25 +0200

    Add API file

---
+++ drupalauth4ssp.api.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Hooks provided by the DrupalAuth for simpleSAMLphp.
+ */
+
+/**
+ * This hook provides ability to alter user attributes.
+ *
+ * @param array $attributes
+ *   Array of attributes that are defined in "SSP_PATH/config/authsources.php".
+ *
+ * @param object $user
+ *   User object.
+ */
+function hook_drupalauth_attributes_alter(&$attributes, &$user) {
+  $attributes['my_attribute'] = 'value';
+}
