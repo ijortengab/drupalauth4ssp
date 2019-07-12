c593943e2c638752a222272f9b4702348289a5cc
Kirill Roskolii <kirill.roskolii@catalyst.net.nz>
2019-07-12 14:10:42 +1200

    Issue #3066359 by RoSk0: DrupalAuth::External fails because cookie is not set after user authentication with cookie.

--- src/EventSubscriber/DrupalAuthForSSPSubscriber.php
+++ src/EventSubscriber/DrupalAuthForSSPSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\drupalauth4ssp\EventSubscriber;
 
+use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\Core\Url;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -13,6 +14,23 @@ use Symfony\Component\HttpKernel\KernelEvents;
  * DrupalAuth for SimpleSAMLphp event subscriber.
  */
 class DrupalAuthForSSPSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Account proxy.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $accountProxy;
+
+  /**
+   * Constructs event subscriber.
+   *
+   * @param \Drupal\Core\Session\AccountProxyInterface $account_proxy
+   *   Account proxy.
+   */
+  public function __construct(AccountProxyInterface $account_proxy) {
+    $this->accountProxy = $account_proxy;
+  }
 
   /**
    * Kernel response event handler.
@@ -27,10 +45,23 @@ class DrupalAuthForSSPSubscriber implements EventSubscriberInterface {
       $path = $response->getTargetUrl();
       $frontPage = Url::fromRoute('<front>')->setAbsolute()->toString();
 
-      $isRedirectToFrontPage = ($path === $frontPage && $response->getStatusCode() === Response::HTTP_FOUND);
+      // Redirect after log out.
+      $responseIsHttpFound = $response->getStatusCode() === Response::HTTP_FOUND;
+      $isRedirectToFrontPage = ($path === $frontPage && $responseIsHttpFound);
       $destination = &drupal_static('drupalauth4ssp_user_logout');
       if ($isRedirectToFrontPage && !empty($destination)) {
         $response->setTargetUrl($destination);
+        $event->stopPropagation();
+        return;
+      }
+
+      // If this was request to login and user was authenticated by cookie.
+      $returnTo = $event->getRequest()->query->get('ReturnTo');
+      $isLoginRequest = $event->getRequest()->attributes->get('_route') === 'user.login';
+      if ($isLoginRequest && $responseIsHttpFound && $returnTo) {
+        drupalauth4ssp_set_user_cookie($this->accountProxy);
+        $response->setTargetUrl($returnTo);
+        $event->stopPropagation();
       }
     }
   }
